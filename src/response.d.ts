7d6a8d264d037f7e140361dfeb72994bc081536a
rickali <rickali@tutanota.com>
2022-04-02 15:27:03 +0430

    response.d.ts: added render

--- src/response.d.ts
+++ src/response.d.ts
@@ -4,42 +4,51 @@ import http from "http"
  * pejvak response object
  */
 export default class pejvakResponse extends http.ServerResponse {
-    #setContentType(value): null;
-    /**
-     * 
-     * @param {Number} statusCode http status code
-     * @returns {pejvakResponse} for chain calls
-     */
-    status(statusCode: String): pejvakResponse;
+	#setContentType(value): null;
+	/**
+	 * 
+	 * @param {Number} statusCode http status code
+	 * @returns {pejvakResponse} for chain calls
+	 */
+	status(statusCode: String): pejvakResponse;
 
-    /**
-     * 
-     * @param {string} data
-     * @returns {pejvakResponse} for chain calls
-     */
-    send(data: string): pejvakResponse;
+	/**
+	 * 
+	 * @param {string} data
+	 * @returns {pejvakResponse} for chain calls
+	 */
+	send(data: string): pejvakResponse;
 
-    /**
-     * 
-     * @param {Object} data json or binary
-     * @returns {pejvakResponse} for chain calls
-     */
-    send(data: Object): pejvakResponse;
+	/**
+	 * render a .pejvakhtml file with a .template file
+	 * @param {string} file 
+	 * @param {string} template 
+	 * @param {Object} settings 
+	 * @param {Object} model 
+	 */
+	render(file: string, template: srting, settings: Object, model: Object);
 
-    /**
-     * 
-     * @param {string} name cookie name
-     * @param {string} value cookie value
-     * @param {{ httpOnly: boolean, secure: boolean, maxAge: number, domain: string, path: string, expires: string, sameSite: [Strict, Lax, None] }} options {httpOnly, secure, maxAge, domain, path, expires, sameSite: [Strict, Lax, None]}
-     */
-    setCookie(name: string, value: string, options:
-        {
-            httpOnly: boolean, secure: boolean, maxAge: number,
-            domain: string, path: string, expires: string, sameSite: [Strict, Lax, None]
-        }): pejvakResponse;
+	/**
+	 * 
+	 * @param {Object} data json or binary
+	 * @returns {pejvakResponse} for chain calls
+	 */
+	send(data: Object): pejvakResponse;
 
-    /**
-     * end of http response
-     */
-    end(): void;
+	/**
+	 * 
+	 * @param {string} name cookie name
+	 * @param {string} value cookie value
+	 * @param {{ httpOnly: boolean, secure: boolean, maxAge: number, domain: string, path: string, expires: string, sameSite: [Strict, Lax, None] }} options {httpOnly, secure, maxAge, domain, path, expires, sameSite: [Strict, Lax, None]}
+	 */
+	setCookie(name: string, value: string, options:
+		{
+			httpOnly: boolean, secure: boolean, maxAge: number,
+			domain: string, path: string, expires: string, sameSite: [Strict, Lax, None]
+		}): pejvakResponse;
+
+	/**
+	 * end of http response
+	 */
+	end(): void;
 }
--- src/response.js
+++ src/response.js
@@ -4,90 +4,90 @@ import mime from "./mimetypes.js"
 import { isString } from "util";
 
 export class pejvakResponse extends http.ServerResponse {
-    constructor() {
-        super();
-    }
-    setContentType(value) {
-        if (this.getHeader("content-type") == undefined)
-            this.setHeader("content-type", value);
-    }
-    status(statusCode) {
-        this.statusCode = statusCode;
-        return this;
-    }
-    send(body) {
-        switch (typeof body) {
-            case "string":
-                this.setContentType(mime.find("html") + "; charset=utf-8");
-                break;
-            case "object":
-                if (Buffer.isBuffer(body))
-                    this.setContentType(mime.find("bin"));
-                else {
-                    this.setContentType(mime.find("json"));
-                    body = JSON.stringify(body);
-                }
-                break;
-        }
-        this.setHeader('Content-Length', Buffer.byteLength(body, 'utf-8'));
-        this.write(body);
-        return this;
-    }
-    // json(body) {
-    //     this.#setContentType(mime.find("json"));
-    //     return JSON.stringify(body);
-    // }
-    render(file, template, settings, model) {
-        renderFile(file, template, settings, model).then(result => {
-            this.status(200).send(result).end();
-            // this.writeHead(200, { "Content-Type": "text" });
-            // this.write(result);
-            // this.end();
-        }).catch(err => {
-            this.emit("error", err);
-            // if (err.code == 'ENOENT')
-            //     this.emit("error", new pejvakHttpError(404));
-            // else
-            //     this.emit("error", err);
-        });
-    }
-    setCookie(name, value, options = {}) {
-        let c = `${name}=${value}`;
+	constructor() {
+		super();
+	}
+	setContentType(value) {
+		if (this.getHeader("content-type") == undefined)
+			this.setHeader("content-type", value);
+	}
+	status(statusCode) {
+		this.statusCode = statusCode;
+		return this;
+	}
+	send(body) {
+		switch (typeof body) {
+			case "string":
+				this.setContentType(mime.find("html") + "; charset=utf-8");
+				break;
+			case "object":
+				if (Buffer.isBuffer(body))
+					this.setContentType(mime.find("bin"));
+				else {
+					this.setContentType(mime.find("json"));
+					body = JSON.stringify(body);
+				}
+				break;
+		}
+		this.setHeader('Content-Length', Buffer.byteLength(body, 'utf-8'));
+		this.write(body);
+		return this;
+	}
+	// json(body) {
+	//     this.#setContentType(mime.find("json"));
+	//     return JSON.stringify(body);
+	// }
+	render(file, template, settings, model) {
+		renderFile(file, template, settings, model).then(result => {
+			this.status(200).send(result).end();
+			// this.writeHead(200, { "Content-Type": "text" });
+			// this.write(result);
+			// this.end();
+		}).catch(err => {
+			this.emit("error", err);
+			// if (err.code == 'ENOENT')
+			//     this.emit("error", new pejvakHttpError(404));
+			// else
+			//     this.emit("error", err);
+		});
+	}
+	setCookie(name, value, options = {}) {
+		let c = `${name}=${value}`;
 
-        if (options.httpOnly)
-            c += "; HttpOnly";
-        if (options.secure)
-            c += "; Secure";
-        if (options.maxAge) {
-            if (isNaN(options.maxAge))
-                throw new TypeError("maxAge option must be a number");
-            c += `; Max-Age=${options.maxAge}`;
-        }
-        if (options.domain)
-            c += `; Domain=${options.domain}`;
-        if (options.path)
-            c += `; Path=${options.path}`;
-        if (options.expires) {
-            if (typeof options.expires.toUTCString != "function")
-                throw new TypeError("expires option must be a valid Date");
-            c += `; Expires=${options.expires.toUTCString()}`;
-        }
-        if (options.sameSite) {
-            if (typeof options.sameSite !== "string")
-                throw new TypeError("sameSite option must be choosed from [Strict, Lax, None]");
-            c += `; SameSite=${options.sameSite[0].toUpperCase() + options.sameSite.substring(1).toLowerCase()}`;
-        }
+		if (options.httpOnly)
+			c += "; HttpOnly";
+		if (options.secure)
+			c += "; Secure";
+		if (options.maxAge) {
+			if (isNaN(options.maxAge))
+				throw new TypeError("maxAge option must be a number");
+			c += `; Max-Age=${options.maxAge}`;
+		}
+		if (options.domain)
+			c += `; Domain=${options.domain}`;
+		if (options.path)
+			c += `; Path=${options.path}`;
+		if (options.expires) {
+			if (typeof options.expires.toUTCString != "function")
+				throw new TypeError("expires option must be a valid Date");
+			c += `; Expires=${options.expires.toUTCString()}`;
+		}
+		if (options.sameSite) {
+			if (typeof options.sameSite !== "string")
+				throw new TypeError("sameSite option must be choosed from [Strict, Lax, None]");
+			c += `; SameSite=${options.sameSite[0].toUpperCase() + options.sameSite.substring(1).toLowerCase()}`;
+		}
 
-        //----append to previous added cookies
-        let _setcookie = [];
-        let h = this.getHeader("Set-Cookie");
-        if (typeof h == "string")
-            _setcookie.push(h);
-        else if (Array.isArray(h))
-            _setcookie = h;
-        _setcookie.push(c);
-        this.setHeader("Set-Cookie", _setcookie);
+		//----append to previous added cookies
+		let _setcookie = [];
+		let h = this.getHeader("Set-Cookie");
+		if (typeof h == "string")
+			_setcookie.push(h);
+		else if (Array.isArray(h))
+			_setcookie = h;
+		_setcookie.push(c);
+		this.setHeader("Set-Cookie", _setcookie);
 
-        return this;
-    }
+		return this;
+	}
 }
